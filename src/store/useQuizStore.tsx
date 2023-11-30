00b9aaf99ff287dbfa1754f69d6262bc269c3964
JOON HYUK LEE <anshqhsh.dev@gmail.com>
2023-12-01 03:22:46 +0900

    feat: timer 값 store관리

--- src/store/useQuizStore.tsx
+++ src/store/useQuizStore.tsx
@@ -4,11 +4,15 @@ import { create } from 'zustand';
 interface IQuizStore {
   quizList: IQuizList[];
   setQuizList: (list: IQuizList[]) => void;
+  timerValue: number;
+  setTimerValue: () => void;
 }
 
-const useQuizStore = create<IQuizStore>((set) => ({
+const useQuizStore = create<IQuizStore>((set, get) => ({
   quizList: [],
   setQuizList: (list: IQuizList[]) => set({ quizList: list }),
+  timerValue: 0,
+  setTimerValue: () => set({ timerValue: get().timerValue + 1 }),
 }));
 
 export default useQuizStore;
