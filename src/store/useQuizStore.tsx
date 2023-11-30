43a08a203561eeeadb13df1123e5e4a29c09829f
JOON HYUK LEE <anshqhsh.dev@gmail.com>
2023-12-01 03:49:40 +0900

    feat: quiz페이지 관리 값을 useQuizStore 에서 관리

--- src/store/useQuizStore.tsx
+++ src/store/useQuizStore.tsx
@@ -1,11 +1,18 @@
 import { IQuizList } from '@/types/quiz';
 import { create } from 'zustand';
 
+interface IQuizProgress {
+  currentQuizIdx: number;
+  answers: boolean[];
+}
+
 interface IQuizStore {
   quizList: IQuizList[];
   setQuizList: (list: IQuizList[]) => void;
   timerValue: number;
   setTimerValue: () => void;
+  quizProgress: IQuizProgress;
+  setQuizProgress: (answer: boolean) => void;
 }
 
 const useQuizStore = create<IQuizStore>((set, get) => ({
@@ -13,6 +20,18 @@ const useQuizStore = create<IQuizStore>((set, get) => ({
   setQuizList: (list: IQuizList[]) => set({ quizList: list }),
   timerValue: 0,
   setTimerValue: () => set({ timerValue: get().timerValue + 1 }),
+  quizProgress: { currentQuizIdx: 0, answers: [] },
+  setQuizProgress: (answer) => {
+    const currentProgress = get().quizProgress;
+    const newAnswers = [...currentProgress.answers, answer];
+    const nextIndex = currentProgress.currentQuizIdx + 1;
+    set({
+      quizProgress: {
+        currentQuizIdx: nextIndex,
+        answers: newAnswers,
+      },
+    });
+  },
 }));
 
 export default useQuizStore;
