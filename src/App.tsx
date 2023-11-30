0427a34c7ba2a38c0aa3c005f0fb637e2ac4d667
JOON HYUK LEE <anshqhsh.dev@gmail.com>
2023-12-01 00:05:11 +0900

    chore: result 페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages';
 import QuizPage from './pages/quiz';
+import ResultPage from './pages/result';
 
 function App() {
   return (
     <Routes>
       <Route path="/quiz" element={<QuizPage />} />
+      <Route path="/result" element={<ResultPage />} />
       <Route path="/" element={<Home />} />
       <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
