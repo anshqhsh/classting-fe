95589c4ab1e53505f850a04c7c515c4817d518e6
JOON HYUK LEE <anshqhsh.dev@gmail.com>
2023-12-01 16:17:20 +0900

    feat: Doughnut 차트 추가

---
+++ src/components/Charts/DoughnetChart/index.tsx
@@ -0,0 +1,50 @@
+import { useMemo } from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import { COLORS } from '@/constants/style';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, TooltipItem } from 'chart.js/auto';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+function DoughnutChart({
+  correctCount,
+  incorrectCount,
+  totalLength,
+}: {
+  correctCount: number;
+  incorrectCount: number;
+  totalLength: number;
+}) {
+  const chartDataAndOption = useMemo(() => {
+    const data = {
+      labels: ['정답 갯수', '오답 갯수'],
+      datasets: [
+        {
+          label: '퀴즈 결과',
+          data: [correctCount, incorrectCount],
+          backgroundColor: [COLORS.COLOR_GREEN_MAIN, COLORS.COLOR_PURPLE_MAIN],
+        },
+      ],
+    };
+    const options = {
+      plugins: {
+        tooltip: {
+          callbacks: {
+            label(context: TooltipItem<'doughnut'>) {
+              return ` : ${context.formattedValue} 개`;
+            },
+            footer(context: TooltipItem<'doughnut'>[]) {
+              return ` ${((Number(context[0].formattedValue) / totalLength) * 100).toFixed(2)}%`;
+            },
+          },
+        },
+      },
+    };
+    return { data, options };
+  }, [correctCount, incorrectCount, totalLength]);
+
+  return <Doughnut data={chartDataAndOption.data} options={chartDataAndOption.options} />;
+}
+
+export default DoughnutChart;
